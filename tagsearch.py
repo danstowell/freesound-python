760fb783e26dc859e4c5498ad7f48b4a42b48b9e
Dan Stowell <danstowell@users.sourceforge.net>
2013-07-11 12:06:17 +0100

    minor refactor

--- tagsearch.py
+++ tagsearch.py
@@ -8,6 +8,7 @@ Freesound.set_api_key(freesound_api_key)
 ##################################
 # settings:
 tagsearch = "field-recording"
+outfolder = 'soundsgot'
 maxpages = 3
 audiotype = "wav"
 minduration = 10
@@ -27,13 +28,13 @@ for whichpage in range(min(maxpages, pager['num_pages'])):
 	for s in pager['sounds']:
 		thesound = Sound.get_sound(s['id'])
 		print "%i %s %s" % (s['id'], s['original_filename'], s['url'])
-		outpath = "%i.%s" % (s['id'], audiotype)
-		if os.path.isfile("soundsgot/%s" % outpath):
+		outfname = "%i.%s" % (s['id'], audiotype)
+		if os.path.isfile("%s/%s" % (outfolder, outfname)):
 			print "           already got"
 		else:
-			thesound.retrieve('soundsgot', outpath)
+			thesound.retrieve(outfolder, outfname)
 			# we also write metadata to file
-			jsonf = open("soundsgot/%i.json" % s['id'], 'w')
+			jsonf = open("%s/%i.json" % (outfolder, s['id']), 'w')
 			# we can't serialise the object directly, so add some of its keys to the search result which we'll serialise
 			for key in ['license', 'samplerate', 'channels', 'geotag']:
 				try:
