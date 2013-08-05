01ef9cc5dae03db5597dc6149c101ee6cce07a32
Daniel Frank Stowell <dans@octave.eecs.qmul.ac.uk>
2013-08-05 12:27:05 +0100

    startpage; and sleep(1)

--- tagsearch.py
+++ tagsearch.py
@@ -1,5 +1,6 @@
 import os.path
 import json
+import time
 
 from freesound import Freesound, Sound
 from localdata import freesound_api_key
@@ -9,9 +10,10 @@ Freesound.set_api_key(freesound_api_key)
 # settings:
 tagsearch = "field-recording"
 outfolder = 'soundsgot'
-maxpages = 3
+maxpages = 1e99
 audiotype = "wav"
 minduration = 10
+startpage = 0
 
 ##################################
 # let's go
@@ -25,16 +27,20 @@ print "Num pages  : %i" % pager['num_pages'  ]
 
 for whichpage in range(min(maxpages, pager['num_pages'])):
 	print "----- PAGE %i -----" % (whichpage + 1)
+	if (whichpage +1) < startpage:
+		pager.next()
+		continue
 	for s in pager['sounds']:
 		thesound = Sound.get_sound(s['id'])
 		print "%i %s %s" % (s['id'], s['original_filename'], s['url'])
 		outfname = "%i.%s" % (s['id'], audiotype)
-		if os.path.isfile("%s/%s" % (outfolder, outfname)):
+		jsonfpath = "%s/%i.json" % (outfolder, s['id'])
+		if os.path.isfile(jsonfpath):
 			print "           already got"
 		else:
 			thesound.retrieve(outfolder, outfname)
 			# we also write metadata to file
-			jsonf = open("%s/%i.json" % (outfolder, s['id']), 'w')
+			jsonf = open(jsonfpath, 'w')
 			# we can't serialise the object directly, so add some of its keys to the search result which we'll serialise
 			for key in ['license', 'samplerate', 'channels', 'geotag']:
 				try:
@@ -43,6 +49,7 @@ for whichpage in range(min(maxpages, pager['num_pages'])):
 					pass
 			jsonf.write(json.dumps(s))
 			jsonf.close()
+			time.sleep(1)
 
 	print
 	pager.next()
